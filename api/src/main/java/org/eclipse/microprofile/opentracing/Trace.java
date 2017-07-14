9c3b15fc25b1c4dba93e27e282d61937d4ab66d4
Steve Fontes <steve.m.fontes@gmail.com>
2017-07-14 15:40:13 -0400

    Fill in the initial content of the repository
    Signed-off-by: Steve Fontes <steve.m.fontes@gmail.com>

---
+++ api/src/main/java/org/eclipse/microprofile/opentracing/Trace.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2017 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.eclipse.microprofile.opentracing;
+
+import javax.enterprise.util.Nonbinding;
+import javax.interceptor.InterceptorBinding;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.Documented;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Define the InterceptorBinding that wraps methods to be traced.
+ * @author <a href="mailto:steve.m.fontes@gmail.com">Steve Fontes</a>
+ *
+ */
+
+@Documented
+@InterceptorBinding
+@Target({ TYPE, METHOD })
+@Retention(RUNTIME)
+public @interface Trace {
+    /** @return the name of this trace point. */
+    @Nonbinding
+    String name() default "";
+    /** @return the relationship of this Span to the current Span. */
+    @Nonbinding
+    String relationship() default "child_of";
+}
---
+++ tck/src/main/java/org/eclipse/microprofile/opentracing/tck/application/TestMethodAnnotationApp.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2017 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.eclipse.microprofile.opentracing.tck.application;
+
+import org.eclipse.microprofile.opentracing.Trace;
+
+
+/**
+ * Application where @Trace annotation is added at the Method level.
+ *
+ * @author Steve Fontes
+ *
+ */
+public class TestMethodAnnotationApp {
+
+    /**
+     * @return name of serviceEnpoint.
+     */
+    @Trace
+    public String serviceEndpointA() {
+        return "Invoked TestMethodAnnotationApp.serviceEndpointA";
+    }
+
+    /**
+     * @return name of serviceEnpoint.
+     */
+    public String serviceEndpointB() {
+        return "Invoked TestMethodAnnotationApp.serviceEndpointB";
+    }
+
+    /**
+     * @return name of serviceEnpoint.
+     */
+    @Trace(name = "MethodAnnotated.endpointC")
+    public String serviceEndpointC() {
+        return "Invoked TestMethodAnnotationApp.serviceEndpointC";
+    }
+
+    /**
+     * @return name of serviceEnpoint.
+     */
+    @Trace(relationship = "follows_from")
+    public String serviceEndpointD() {
+        return "Invoked TestMethodAnnotationApp.serviceEndpointD";
+    }
+
+    /**
+     * @return name of serviceEnpoint.
+     */
+    @Trace(name = "MethodAnnotated.endpointE", relationship = "follows_from")
+    public String serviceEndpointE() {
+        return "Invoked TestMethodAnnotationApp.serviceEndpointE";
+    }
+}
