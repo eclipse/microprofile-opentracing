c636eb9b14acc2b796a93dbb4a0d92b346136448
Pavol Loffay <ploffay@redhat.com>
2018-05-03 19:28:59 +0200

    Remove spaces from code
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- api/src/main/java/org/eclipse/microprofile/opentracing/Traced.java
+++ api/src/main/java/org/eclipse/microprofile/opentracing/Traced.java
@@ -34,16 +34,16 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * This annotation allows fine-tuned control over which classes and methods
  * create OpenTracing spans. By default, all JAX-RS methods implicitly have
  * this annotation.
- * 
+ *
  * This annotation applies to a class or a method. When applied to a class, this
  * annotation is applied to all methods of the class. If the annotation is
  * applied to a class and method then the annotation applied to the method takes
  * precedence. The annotation starts a Span at the beginning of the method, and
  * finishes the Span at the end of the method.
- * 
+ *
  * This annotation also has {@code InterceptorBinding} for frameworks to
  * process all of each application's explicit {@code Traced} annotations.
- * 
+ *
  * @author <a href="mailto:steve.m.fontes@gmail.com">Steve Fontes</a>
  */
 @Documented
@@ -57,13 +57,13 @@ public @interface Traced {
      * methods to disable creation of a Span for those methods. By default all
      * JAX-RS endpoint methods are traced. To disable Span creation of a
      * specific JAX-RS endpoint, the @Traced(false) annotation can be used.
-     * 
+     *
      * When the <code>@Traced(false)</code> annotation is used for a JAX-RS
      * endpoint method, the upstream SpanContext will not be extracted. Any
      * Spans created, either automatically for outbound requests, or explicitly
      * using an injected Tracer, will not have an upstream parent Span in the
      * Span hierarchy.
-     * 
+     *
      * @return whether this method should be traced.
      */
     @Nonbinding
@@ -80,7 +80,7 @@ public @interface Traced {
      * operationName is specified on a class, that operationName will be used
      * for all methods of the class unless a method explicitly overrides it with
      * its own operationName.
-     * 
+     *
      * @return the name to give the Span for this trace point.
      */
     @Nonbinding
